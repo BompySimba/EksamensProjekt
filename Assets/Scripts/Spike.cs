802a131321b6923664f44488d724953cf8392d67
Carrot <dianarezaeiii@gmail.com>
2026-03-26 14:03:34 +0100

    Level 2 Start

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    private void OnTriggerEnter2D(Collider2D collisionInfo)
+    {
+        if(collisionInfo.tag == "Player")
+        {
+            gameManager.RestartLevel();
+        }
+    }
+}
